1d79934ff2c6c6458c9fe833d0916602ff225834
Nilashish Roy <nilashishroyjoy@gmail.com>
2022-03-07 04:40:00 +0600

    Added Budget

---
+++ Budget.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Budget
+{
+    public int Id { get; set; }
+    public string Category { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Month { get; set; }
+}
